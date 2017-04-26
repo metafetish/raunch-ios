42d3c1d5787318d0d6305152302c0695059a7cb4
Blackboxed <email@blackboxed.me>
2017-04-26 09:56:00 -0400

    Created project in Xcode

---
+++ Raunch/Raunch.h
@@ -0,0 +1,19 @@
+//
+//  Raunch.h
+//  Raunch
+//
+//  Created by Blackboxed on 2017-04-26.
+//  Copyright © 2017 Metafetish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Raunch.
+FOUNDATION_EXPORT double RaunchVersionNumber;
+
+//! Project version string for Raunch.
+FOUNDATION_EXPORT const unsigned char RaunchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raunch/PublicHeader.h>
+
+
